5d0390e1136cb3988a62ccf7e50574fb5d21f4eb
vikranthkeerthipati <vkeerthipati@gatech.edu>
2024-02-28 03:21:21 -0500

    backend better work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import { ModeToggle } from "@/components/ModeToggle";
 import ImageList from "@/components/imageList";
 import Header from "./header";
 import NextLink from "next/link";
-import { createClient } from '@supabase/supabase-js'
-export const dynamic = 'force-dynamic'
+import { createClient } from "@supabase/supabase-js";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  // const demos = await getProjects();
-
+  const demos = await getProjects();
   return (
     <div className="[ overflow-x-none min-h-screen ]">
       <Header />
@@ -24,7 +23,9 @@ export default async function Home() {
                 className="w-full text-center space-y-7"
                 style={{ fontSize: "5vw", whiteSpace: "nowrap" }}
               >
-                <span className={`${drukWide.className} dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-r dark:from-white dark:to-gray-500 fill-primary ship-it`}>
+                <span
+                  className={`${drukWide.className} dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-r dark:from-white dark:to-gray-500 fill-primary ship-it`}
+                >
                   ship it
                 </span>
                 <h2
@@ -55,58 +56,41 @@ export default async function Home() {
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="w-full flex justify-center z-20">
-
-          </div>
-          {/* <ImageList demos={demos} /> */}
+          <div className="w-full flex justify-center z-20"></div>
+          <ImageList demos={demos} />
         </div>
-
       </main>
     </div>
   );
 }
 
+async function getProjects() {
+  // const supabase = createClient('https://wrlispjjwzomjvomdrbm.supabase.co', "")
 
-// async function getProjects() {
-//   const supabase = createClient('https://wrlispjjwzomjvomdrbm.supabase.co', "")
+  const res = await fetch(
+    "https://api.airtable.com/v0/applTePF0yE2evQhL/Projects%20(for%20website)",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+      },
+    }
+  );
 
-//   const res = await fetch(
-//     "https://api.airtable.com/v0/applTePF0yE2evQhL/Projects%20(for%20website)",
-//     {
-//       headers: {
-//         Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-//       },
-//     }
-//   );
-//   const data = await res.json();
-//   console.log(data)
-//   const recs = data["records"].map(async (record: any) => {
-//     const id = record['id']
-//     const oneLiner = record.fields["One-liner"];
-//     const name = record.fields["Name"];
-//     const image = record.fields["Media"][0]["url"];
-//     const imageData = await fetch(image).then(res => res.blob())
-//     const base64data = URL.createObjectURL(imageData)
-    
-//     let { data, error } = await supabase
-//   .storage
-//     .getBucket('airtableImagesBucket')
-//     if(data == null) {
-//       let { data, error } = await supabase
-//         .storage
-//         .createBucket('airtableImagesBucket', {
-//           public: false,
-//         })
-//         console.log(data)
-//         console.log(error)
-//     }
-//     console.log(data)
-//     // console.log(error)
-    
-//     // if()
-//     return { id, oneLiner, name, image };
-//   });
+  const data = await res.json();
+  const recs = Promise.all(
+    data["records"].map(async (record: any) => {
+      const id = record["id"];
+      const oneLiner = record.fields["One-liner"];
+      const name = record.fields["Name"];
+      const image = record.fields["Media"][0]["url"];
+      const response = await fetch(image);
+      let blob = await response.blob();
+      let buffer = Buffer.from(await blob.arrayBuffer());
+      const base64url =
+        "data:" + blob.type + ";base64," + buffer.toString("base64");
+      return { id, oneLiner, name, base64url };
+    })
+  );
 
-
-//   return recs;
-// }
+  return recs;
+}
